2c399afcba5caac0fd45c4d6fa993788be1e9543
Igor-Polonskiy <53859088+Igor-Polonskiy@users.noreply.github.com>
2021-03-09 23:03:52 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [str[0]]
+    let result 
+
+    for (let i = 1; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (bracketsConfig[j].includes(str[i])) {
+                if(bracketsConfig[j].includes(arr[arr.length-1])){
+                  if(bracketsConfig[j][0]==arr[arr.length-1]&&bracketsConfig[j][1]==str[i]){
+                    arr.pop()
+                  }else arr.push(str[i])
+                }else arr.push(str[i])
+            }
+        }
+    }
+
+    if (arr.length == 0) {
+        result = true
+    } else result = false
+
+    return result
 }
